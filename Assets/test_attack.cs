9531e43479b14a05469bb06dd4f2eee22f466fdb
Kapom310 <124363815+Kapom310@users.noreply.github.com>
2023-02-04 03:26:17 +0700

    edit: example health component

---
+++ Assets/test_attack.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test_attack : MonoBehaviour
+{
+    public GameObject target;
+    HealthComponent health;
+
+
+    void Start()
+    {
+         health = target.GetComponent<HealthComponent>();
+        if(health != null)
+        {
+            HealthComponent.OnOutOfHealth += printDie;
+        StartCoroutine(attack());
+        }
+    }
+
+    IEnumerator attack()
+    {
+            yield return new WaitForSeconds(1);
+        health.TakeDamage(1);
+        print(health.GetRemainingHealth());
+
+        StartCoroutine(attack());
+    }
+
+    void printDie()
+    {
+        print("Die");
+    }
+}
