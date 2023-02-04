b2d48073eeadcf9b478f5c1d0929fbf55b908161
FadeZx <50790210+FadeZx@users.noreply.github.com>
2023-02-04 23:01:38 +0700

    Player animation

--- Assets/Script/MC_control.cs
+++ Assets/Script/MC_control.cs
@@ -9,13 +9,12 @@ public class MC_control : MonoBehaviour
     // Start is called before the first frame update
     public float speed = 5f;
     public Rigidbody2D rb;
-    //public Animator animator;
+     Animator animator;
     public Vector2 movement;
 
 
     
     public bool collision;
-    public Transform Checker;
     public Collider2D MC_collider;
     public float groundradius = 0.2f;
 
@@ -27,27 +26,45 @@ public class MC_control : MonoBehaviour
         MC_collider = GetComponent<Collider2D>();
         rb = GetComponent<Rigidbody2D>();
         ren = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
 
     void FixedUpdate()//Input
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        if(movement != Vector2.zero)
+        {
+            bool success = Move(movement);
+            if(!success)
+            {
+                success = Move(new Vector2(movement.x, 0));
+                if(!success)
+                {
+                    success = Move(new Vector2(0,movement.y));
+                }
+            }
+        }
+        // movement.x = Input.GetAxisRaw("Horizontal");
+        // movement.y = Input.GetAxisRaw("Vertical");
 
-        //animator.SetFloat("Horizontal", movement.x);
-        //animator.SetFloat("speed", movement.sqrMagnitude);
+        // animator.SetFloat("Horizontal", movement.x);
+        animator.SetBool("isMOving", success);
+        
 
     }
 
     // Update is called once per frame
     void Update()//movement
     {
-        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime); 
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);  
         if(movement.y ==1)
         {
             //ren.renderingLayerMask = 1 ;
         }
 
+        
+    }
+    void Move()
+    {
         
     }
 
