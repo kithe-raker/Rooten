60dc4458d6a7c6b3d06972a5f1dbf7c8aa52929f
Kapom310 <124363815+Kapom310@users.noreply.github.com>
2023-02-05 02:09:39 +0700

    player complete

---
+++ Assets/Script/PlayerDanger.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerDanger : MonoBehaviour
+{
+    HealthComponent health;
+    public int sceneIndex;
+    public SpriteRenderer sprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = this.gameObject.GetComponent<HealthComponent>();
+        health.OnOutOfHealth += LoadScene;
+        health.OnTakeDamage += TakeDamage;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LoadScene()
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    void TakeDamage(int damage)
+    {
+        StartCoroutine(FlashRed());
+    }
+
+    public IEnumerator FlashRed()
+    {
+        sprite.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        sprite.color = Color.white;
+    }
+}
--- Assets/Script/WeaponHitBox.cs
+++ Assets/Script/WeaponHitBox.cs
@@ -8,17 +8,17 @@ public class WeaponHitBox : MonoBehaviour
     public int damage = 1;
     HealthComponent health;
     private float attackTime = 0f;
-    private float attackDelay= 1f;
-   void OnTriggerEnter2D(Collider2D target)
-   {    
+    private float attackDelay = 1f;
+    void OnTriggerEnter2D(Collider2D target)
+    {
         if (target.gameObject.tag == "Enemy")
         {
             health = target.GetComponent<HealthComponent>();
-            
-                //Debug.Log(target.gameObject.name);
-                Debug.Log("Hit!! Enemy");
-                health.TakeDamage(damage);
-                attackTime = 0f;
+
+            //Debug.Log(target.gameObject.name);
+            Debug.Log("Hit!! Enemy");
+            health.TakeDamage(damage);
+            attackTime = 0f;
         }
     }
 }
