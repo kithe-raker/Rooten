2c36700debb9b89c0b1474f197fbd8c6c65e418d
FadeZx <50790210+FadeZx@users.noreply.github.com>
2023-02-04 16:34:32 +0700

    Attack cooldown

--- Assets/Script/WeaponHitBox.cs
+++ Assets/Script/WeaponHitBox.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class WeaponHitBox : MonoBehaviour
 {
-    public GameObject target;
+    //public GameObject target;
     public int damage = 1;
     HealthComponent health;
    void OnTriggerEnter2D(Collider2D target)
