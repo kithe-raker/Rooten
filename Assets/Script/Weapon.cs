66fc3e1e940d2110619774ae8a6f9a1a73d87c9b
Kapom310 <124363815+Kapom310@users.noreply.github.com>
2023-02-05 11:57:13 +0700

    edit: update weapon collision duration

--- Assets/Script/Weapon.cs
+++ Assets/Script/Weapon.cs
@@ -30,7 +30,7 @@ public class Weapon : MonoBehaviour
     IEnumerator Attack()
     {
         weaponCollider.enabled = true;
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(0.3f);
         weaponCollider.enabled = false;
         attacksLeft--;
         if (attacksLeft == 0)
